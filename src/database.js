ba3e80bbc344272919b9bc5ea8e7aa0e18408334
Oni12 <76744524+Oni12@users.noreply.github.com>
2021-06-29 14:59:16 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Oni:Soficita1@@cluster0.svxu4.mongodb.net/oni?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://oni:oni@cluster0.svxu4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
